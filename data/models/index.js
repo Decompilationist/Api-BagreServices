54aaf3e1f70c3d74c3872a32661f4e5587e58e02
Decompilationist <verboty@gmail.com>
2023-05-04 09:28:11 -0300

    Rotas de usuário

---
+++ data/models/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const users = require ('./schemas/users');
+
+const model = mongoose.model.bind(mongoose);
+
+module.exports = {
+  Users: model('Users', users)
+}
--- index.js
+++ index.js
@@ -5,28 +5,36 @@ const morgan = require('morgan');
 const cookiepParser = require('cookie-parser');
 const cors = require('cors');
 const destinationsRouter = require('./destinations');
-const usersRouter = require('./users');
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(cors());
-
-// Conexão com o banco de dados
-db.connect();
-
-// Rotas para os destinos
-app.use('/api/destinations', destinationsRouter);
-
-// Rotas para os usuários
-app.use('/api/users', usersRouter);
-
-// Iniciar o servidor
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
+const routes = require('./routes') 
 
 
 const { env: { PORT, MONGO_URL: url }, argv: [, , port = PORT || 3001], } = process;
 app = express()
+
+(async () => {
+  await mongoose.connect(url, { useNewUrlParser: true });
+
+  // express
+  app.use(cors());
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(morgan('dev'));
+
+  // routes 
+  app.use(routes);
+
+  // app.use(function (req, res, next) {
+  //     res.status(404).json({ error: 'Not found.' });
+  // });
+
+  // Error catching endware.
+  app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+      const status = err.status || 500;
+      const message = err.message || err;
+      console.error(err);
+      res.status(status).send(message);
+  });
+
+  app.listen(port, () => console.log(`${package.name} ${package.version} na porta ${port}`))
+})();
+
