97a78e73572793259a15de8ee3a4c014c1ff2415
Decompilationist <verboty@gmail.com>
2023-05-04 10:37:51 -0300

    main

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+const postUsers = require('./users/postUsers.js');
+
+
+
+router.use(postUsers);
+
+module.exports = router;
