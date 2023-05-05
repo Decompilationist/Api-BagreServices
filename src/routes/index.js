81d639139d23291fa7bd18e01190e426c8fb02a2
Gustavo Menezes <verboty@gmail.com>
2023-05-05 11:54:59 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const postUsers = require('./users/postUsers.js');
 const getServices = require('./services/getServices');
 const postServices = require('./services/postServices');
 const deleteServices = require('./services/deteleServices.js');
+const updateServices = require('./services/updateServices.js')
 
 
 
@@ -12,5 +13,6 @@ router.use(postUsers);
 router.use(getServices);
 router.use(postServices);
 router.use(deleteServices);
+router.use(updateServices);
 
 module.exports = router;
