63bdd7bb0186367adb58ea8a31bbcab1be604814
Gustavo Menezes <103705903+Decompilationist@users.noreply.github.com>
2023-05-04 22:37:15 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mongodb: {
-        url: 'mongodb+srv://admin:bagremaluco@bagreservices.rqk3ort.mongodb.net/?retryWrites=true&w=majority'
-    },
-    secretKey: 'bagremaluco',
-    apiKeys: {
-        googleMaps: 'chave-da-api-do-google-maps'
-    }
-};
